641858454e8d238a6364673e249f30440510c72d
Bottesz <botyaring@gmail.com>
2023-10-19 12:15:17 +0200

    gyakorlas ciklus

---
+++ main.py
@@ -0,0 +1,11 @@
+import szamolas
+### 1. feladat
+szam1:int = int(input("Kérem, adjon meg egy pozitív egész számot: "))
+szamolas.elsofeladat(szam1)
+### 2. feladat
+szam2:int = int(input("Kérem, adjon meg egy pozitív egész számot: "))
+szamolas.masodikfeladat(szam2)
+### 3. feladat
+szam3:int = int(input("Kérem, adja meg az első számot: "))
+szam4:int = int(input("Kérem, adja meg a második számot: "))
+szamolas.harmadikfeladat(szam3,szam4)
